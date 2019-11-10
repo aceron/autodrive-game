a9ff28417ee717a1774d9c6c6953381256409bd7
aceron <arturo.e.ceron@gmail.com>
2019-11-10 21:19:12 +0900

    Changed code comment

--- cap-screen-game.py
+++ cap-screen-game.py
@@ -30,7 +30,7 @@ button_delay = 0.2
 dir_name = ""
 sample_count = 0
 
-# Get window for capturing sample
+# Get window for sending key events to a window
 print(len(xdo.search_windows(win_name.encode())))
 win_id = xdo.search_windows(win_name.encode())[0]
 print("WIN ID:" + str(win_id))
